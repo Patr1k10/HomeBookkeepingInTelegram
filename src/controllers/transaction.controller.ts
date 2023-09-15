d38f18083182a4791ea3d8f7c2cb693eaa537baf
mymacbook <vladimir.vavulin@icloud.com>
2023-09-15 10:06:07 +0300

    V0.0.0.2

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,11 +1,9 @@
 import { PinoLoggerService } from '../loger/pino.loger.service';
 import { TransactionService } from '../transaction.service';
-import { Transaction } from '../interface/transaction.interface';
 
 export class TransactionController {
   constructor(
     private readonly logger: PinoLoggerService,
     private readonly transactionService: TransactionService,
-  ) {
-  }
+  ) {}
 }
