2f94716d23150555ff364623eda1a4bc99de514d
mymacbook <vladimir.vavulin@icloud.com>
2023-09-26 21:44:00 +0300

    V0.0.P3.0 UA🇺🇦

--- src/app.buttons.ts
+++ src/app.buttons.ts
@@ -2,7 +2,7 @@ import { Markup } from 'telegraf';
 
 export function actionButtonsStart() {
   return Markup.keyboard(
-    [Markup.button.callback('Баланс 💰', 'Баланс'), Markup.button.callback('Транзакция 💸', 'Транзакция')],
+    [Markup.button.callback('Баланс 💰', 'Баланс'), Markup.button.callback('Транзакція 💸', 'Транзакція')],
     {
       columns: 2,
     },
@@ -12,9 +12,9 @@ export function actionButtonsStart() {
 export function actionButtonsTransaction() {
   return Markup.inlineKeyboard(
     [
-      Markup.button.callback('Приход 💹', 'Приход'),
-      Markup.button.callback('Расход 🛍️', 'Расход'),
-      Markup.button.callback('Удаление последних️❌', 'Удаление последних️'),
+      Markup.button.callback('Надходження 💹', 'Приход'),
+      Markup.button.callback('Витрати 🛍️', 'Расход'),
+      Markup.button.callback('Видалення останніх️❌', 'Удаление последних️'),
     ],
     { columns: 2 },
   );
@@ -23,13 +23,13 @@ export function actionButtonsTransaction() {
 export function actionButtonsStatistics() {
   return Markup.inlineKeyboard(
     [
-      Markup.button.callback('За сегодня', 'За сегодня'),
-      Markup.button.callback('За неделю🗓️', 'За неделю'),
-      Markup.button.callback('За месяц🗓️', 'За месяц'),
-      Markup.button.callback('Выбрать месяц📤️', 'Выбрать месяц'),
-      Markup.button.callback('Мои приходы💵', 'Мои приходы'),
-      Markup.button.callback('Мои расходы🧾', 'Мои расходы'),
-      Markup.button.callback('По категории🗃️', 'По категории'),
+      Markup.button.callback('За сьогодні', 'За сегодня'),
+      Markup.button.callback('За тиждень🗓️', 'За неделю'),
+      Markup.button.callback('За місяць🗓️', 'За месяц'),
+      Markup.button.callback('Вибрати місяць📤️', 'Выбрать месяц'),
+      Markup.button.callback('Мої надходження💵', 'Мои приходы'),
+      Markup.button.callback('Мої витрати🧾', 'Мои расходы'),
+      Markup.button.callback('За категорією🗃️', 'По категории'),
     ],
     { columns: 2 },
   );
@@ -44,18 +44,18 @@ export function actionButtonsTransactionNames(transactionNames: string[]) {
 
 export function actionButtonsMonths() {
   const months = [
-    'Январь',
-    'Февраль',
-    'Март',
-    'Апрель',
-    'Май',
-    'Июнь',
-    'Июль',
-    'Август',
-    'Сентябрь',
-    'Октябрь',
-    'Ноябрь',
-    'Декабрь',
+    'Січень',
+    'Лютий',
+    'Березень',
+    'Квітень',
+    'Травень',
+    'Червень',
+    'Липень',
+    'Серпень',
+    'Вересень',
+    'Жовтень',
+    'Листопад',
+    'Грудень',
   ];
   const buttons = months.map((month, index) => Markup.button.callback(month, `Month:${index + 1}`));
   return Markup.inlineKeyboard(buttons, { columns: 3 });
